15ea0bcd4a78b44fae291e81d12985ea85c471df
baejh <m9310142@naver.com>
2024-04-05 10:21:38 +0900

    done

--- ConsoleApplication1/data_Struct_chap03(stack)5.cpp
+++ ConsoleApplication1/data_Struct_chap03(stack)5.cpp
@@ -1,5 +1,5 @@
 /*
-프로그램 내용: 스택을 이용한 후위 표기 변환 프로그램
+프로그램 내용: 스택을 이용한 후위 표기 변환 프로그램 // 후위 표기 합친기
 실습날짜: 2024 - 04 - 05
 학번: 202111001
 이름: 이동재
@@ -30,7 +30,7 @@ void push(element e)
 	if (is_full())
 		error("스택 포화 에러");
 	else
-		data[++top] = e;
+		data[++top] = e; // top을 ++하고 값을 넣음
 }
 
 element pop()
@@ -38,7 +38,7 @@ element pop()
 	if (is_empty())
 		error("스택 공백 에러");
 	else
-		return data[top--];
+		return data[top--]; // top 을 --하고 값을 추출함
 }
 
 element peek()
@@ -49,46 +49,47 @@ element peek()
 		return data[top];
 }
 
-int precedence(char op) 
+int precedence(char op)  // 연산자의 우선순위를 반환하는 함수 ( 높을수록 우선순위가 높다)
 {
 	switch (op) {
 	case '(': case ')': return 0;
 	case '+': case '-': return 1;
 	case '*': case '/': return 2;
 	}
-	return -1;
+	return -1; // 잘못입력한 경우
 }
 
 void infix_to_postfix(char expr[])
 {
+	
 	int i = 0;
-	char c, op;
+	char c, op; // c: 현재, op: 스택
 
 	init_stack();
-	while (expr[i] != '\0')
+	while (expr[i] != '\0') // 문자열이 끝날때 까지 반복
 	{
 		c = expr[i++];
-		if ((c >= '0' && c <= '9'))
+		if ((c >= '0' && c <= '9')) // 피연산자(숫자) 일 경우
 		{
 			printf("%c", c);
 		}
 		else if (c == '(')
 			push(c);
-		else if (c == ')')
+		else if (c == ')') // 닫히는 괄호일 경우
 		{
 			while (is_empty() == 0)
 			{
-				op = pop();
-				if (op == '(') break;
+				op = pop(); // 스택에서 꺼냄
+				if (op == '(') break; // 여는 괄호를 만날때 까지
 				else printf("%c", op);
 			}
 		}
-		else if (c == '+' || c == '-' || c == '*' || c == '/')
+		else if (c == '+' || c == '-' || c == '*' || c == '/') // 연산자일 경우
 		{
-			while (is_empty() == 0)
+			while (is_empty() == 0) // 스택이 비어있다면
 			{
-				op = peek();
-				if (precedence(c) <= precedence(op))
+				op = peek(); // op: 스택의 top에 있는 연산자
+				if (precedence(c) <= precedence(op))  // 현재 선택된 연산자와 스택의 top에있는 연산자의 우선순위를 비교한다.
 				{
 					printf("%c", op);
 					pop();
